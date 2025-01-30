5837f13cba4e4e3596c26f0e704a5d707273d2d0
BhargaviRM <106804698+BhargaviRM@users.noreply.github.com>
2025-01-30 11:34:33 +0530

    Update main.py

--- FastAPI With Sqlalcehmy/Postgresql/app/main.py
+++ FastAPI With Sqlalcehmy/Postgresql/app/main.py
@@ -1,3 +1,10 @@
+
+# for installing FastAPI: pip install "fastapi[standard]"
+# for installing sqlalchemy: pip install sqlalchemy
+# running server: uvicorn main:app --reload
+# http://127.0.0.1:8000 
+# for flagger (http://127.0.0.1:8000/docs)
+
 from fastapi import FastAPI
 from .router import router
 from .database import engine, Base
