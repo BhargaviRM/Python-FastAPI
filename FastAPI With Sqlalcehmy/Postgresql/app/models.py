98df414e87ab7f93e783696bfb544ce7b7de6a52
BhargaviRM <106804698+BhargaviRM@users.noreply.github.com>
2025-01-30 11:39:07 +0530

    Update models.py

--- FastAPI With Sqlalcehmy/Postgresql/app/models.py
+++ FastAPI With Sqlalcehmy/Postgresql/app/models.py
@@ -10,7 +10,7 @@ class Task(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, nullable=False)
-    description = Column(String, nullable=False)
+    description = Column(String)
     completed = Column(Boolean, server_default="FALSE", nullable=False)
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
